1bf3e2c7e5d64d3a2f541d4ebdc01f7e267a2317
cdharmale30 <36369070+cdharmale30@users.noreply.github.com>
2018-12-01 23:09:35 -0800

    Commited

---
+++ src/com/safaritest/SafariTest.java
@@ -0,0 +1,16 @@
+package com.safaritest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class SafariTest {
+	public static void main(String[] args) {
+		WebDriver driver = new SafariDriver();
+		driver.get("http://www.gmail.com");
+
+	
+	}
+
+}
